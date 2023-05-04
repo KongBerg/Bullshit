723feacaf574f8664e8eaf61622f24d66dd4affb
AbduAwais <abdu.awais@outlook.dk>
2023-05-04 10:26:01 +0200

    upj

---
+++ infrastruktur.sql
@@ -0,0 +1,8 @@
+"id","firmaer_navne","metric_tons_pr_year","data_centers_procent","data_centers_emisions"
+1,"Facebook",982000,94.0,923080
+2,"Google",4900000,46.0,2254000
+4,"Amazon",60640000,69.0,41841600
+5,"Netlfix",1100000,78.0,858000
+6,"Microsoft",16400000,37.0,6068000
+7,"Det Danske Landbrug ",10600000,0.0,0
+8,"Hvor meget CO2 10 Hektar rengksov i Panamra kan opsamle.",6200000,NULL,NULL
